7ae290ab61b2097c38f26e962bf8f65ffcc5ee00
richimenendez <richimenen@gmail.com>
2020-04-15 11:20:29 -0600

    [1.0.1] Añado el modulo de registro en el cliente de React, y en el sevidor añado sus metodos para hacerlo funcional. Ricardo Menéndez - 201602916

--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const app = express();
 app.listen(4000, () => {
-    let host = 'localhost';
-    console.log('El servidor esta prendido en http://%s:%s', host, 4000)
-})
+  let host = "localhost";
+  console.log("El servidor esta prendido en http://%s:%s", host, 4000);
+});
 
-app.get("/holamundo",(req,res)=>{
-	res.text("hola mundo");
-})
+app.get("/holamundo", (req, res) => {
+  res.text("hola mundo");
+});
 
+app.get("/almacenaje", (req, res) => {
+  res.text("Almacenaje");
+});
 
-app.get("/almacenaje",(req,res)=>{
-	res.text("Almacenaje");
-})
+app.get("/registro", (req, res) => {
+  res.text("Usuario Registrado!!!");
+});
 
 // SE AJUSTARON LAS VERSIONES xd
