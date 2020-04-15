c0cebc544725d051ea3278ef8b30e64ca75e88cb
Herberth <herberaegueta4@gmail.com>
2020-04-15 00:11:03 -0600

    Agregar  funcionalidad del servidor Herberth Argueta 201602634

--- server/index.js
+++ server/index.js
@@ -4,3 +4,7 @@ app.listen(4000, () => {
     let host = 'localhost';
     console.log('El servidor esta prendido en http://%s:%s', host, 4000)
 })
+
+app.get("/holamundo",(req,res)=>{
+	res.text("hola mundo");
+})
